f9e1fd2d5037521c37dedac89669cb76508fa085
SergeiZahar86 <sergei86zahar@mail.ru>
2020-12-15 20:31:11 +0700

    Добавил таймер. Работает

--- ApplicationViewModel.cs
+++ ApplicationViewModel.cs
@@ -11,6 +11,9 @@ namespace WPF_MVVM_metanit.com
 {
     public class ApplicationViewModel : INotifyPropertyChanged
     {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer;   // Таймер
+
+
         private Phone selectedPhone;
 
         public ObservableCollection<Phone> Phones { get; set; }
@@ -32,7 +35,12 @@ namespace WPF_MVVM_metanit.com
         }
 
 
+        private void OnTimedEvent(Object source, EventArgs e)      // Получение вагонов с сервера по таймеру
+        {
+            Phone phone = new Phone { Title = "iPhone 7", Company = "Apple", Price = 56000 };
+            Phones.Insert(0, phone);
 
+        }
 
         public Phone SelectedPhone
         {
@@ -52,6 +60,12 @@ namespace WPF_MVVM_metanit.com
                 new Phone {Title="Elite x3", Company="HP", Price=56000 },
                 new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
             };
+
+            // Таймер ///////////////////////////////////////
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(OnTimedEvent);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();                    // Стартуем таймер
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
